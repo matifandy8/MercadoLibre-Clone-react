3ff613daf0c1b171f03b819c7a52cf7419113077
matifandy <matifandy@gmail.com>
2020-11-11 21:43:07 -0300

    navbar responsive

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="navbar">
       <nav className="main-nav">
         <div className="main-nav__logo">
           <a className="main-nav__logo-link">Company Name</a>
         </div>
-        <ul className="main-nav__menu">
+        <ul
+          className="main-nav__menu"
+          style={{ transform: open ? "translateX(0px)" : "translateX(-500px)" }}
+        >
           <li className="main-nav__menu-item">
             <a className="main-nav__menu-item-link">Categories</a>
           </li>
@@ -22,6 +28,11 @@ function Navbar() {
             <a className="main-nav__menu-item-link">Mi Carrito</a>
           </li>
         </ul>
+        <MenuIcon
+          onClick={() => setOpen(!open)}
+          className="burger"
+          fontSize="large"
+        />
       </nav>
     </div>
   );
