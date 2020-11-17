239b68f1f69c10c8e159f0bada1425a07f1b81c8
matifandy <matifandy@gmail.com>
2020-11-17 19:53:42 -0300

    stores-brands

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import "./swiper/swiper-bundle.css";
+
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
-      <div className="home__slider"></div>
+      <div className="home__slider">
+        <img src="https://http2.mlstatic.com/optimize/o:f_webp/resources/exhibitors/MLU-envios-gratis/c215a310-1adb-11eb-89df-6f1b33c1157b-home-slider_desktop.jpg" />
+      </div>
+      <h1>Las mejores tiendas te esperan</h1>
+      <div className="home__stores">
+        <div className="home__brand">
+          <div className="home__brand__top">
+            <img src="https://www.lenovo.com/medias/lenovo-thinkpad-e590-front.png?context=bWFzdGVyfHJvb3R8MjQzNTd8aW1hZ2UvcG5nfGgyZi9oMTgvOTg3MTA4MzM3MjU3NC5wbmd8YzNkNGE2YjVjZTJhZDczYzExZDU1MjJiNTQ2ZGJmZWM0ZTAzODRiNGZkNGVlOTZjMWZkOWY3ZDQ3Mjg3MjdkMg" />
+            <img src="https://http2.mlstatic.com/D_Q_NP_897210-MLA26014325691_092017-AE.webp" />
+            <h2>Lenovo</h2>
+          </div>
+          <div className="home__brand__bottom">
+            <img src="https://http2.mlstatic.com/D_Q_NP_758886-MLU42155384479_062020-R.webp" />
+            <img src="https://http2.mlstatic.com/D_Q_NP_852863-MLU42958152306_072020-R.webp" />
+            <img src="https://http2.mlstatic.com/D_Q_NP_647905-MLU42184073149_062020-R.webp" />
+            <a>Ver tienda</a>
+          </div>
+        </div>
+        <div className="home__brand">
+          <img src="https://www.lenovo.com/medias/lenovo-thinkpad-e590-front.png?context=bWFzdGVyfHJvb3R8MjQzNTd8aW1hZ2UvcG5nfGgyZi9oMTgvOTg3MTA4MzM3MjU3NC5wbmd8YzNkNGE2YjVjZTJhZDczYzExZDU1MjJiNTQ2ZGJmZWM0ZTAzODRiNGZkNGVlOTZjMWZkOWY3ZDQ3Mjg3MjdkMg" />
+          <img src="https://http2.mlstatic.com/D_Q_NP_984708-MLA25722192765_072017-AE.webp" />
+          <h2>LG</h2>
+          <img src="" />
+          <img src="" />
+          <img src="" />
+        </div>
+        <div className="home__brand">
+          <img src="https://www.lenovo.com/medias/lenovo-thinkpad-e590-front.png?context=bWFzdGVyfHJvb3R8MjQzNTd8aW1hZ2UvcG5nfGgyZi9oMTgvOTg3MTA4MzM3MjU3NC5wbmd8YzNkNGE2YjVjZTJhZDczYzExZDU1MjJiNTQ2ZGJmZWM0ZTAzODRiNGZkNGVlOTZjMWZkOWY3ZDQ3Mjg3MjdkMg" />
+          <img src="https://http2.mlstatic.com/D_Q_NP_929266-MLA28000008340_082018-AE.webp" />
+          <h2>Huawei</h2>
+          <img src="" />
+          <img src="" />
+          <img src="" />
+        </div>
+        <div className="home__brand">
+          <img src="https://www.lenovo.com/medias/lenovo-thinkpad-e590-front.png?context=bWFzdGVyfHJvb3R8MjQzNTd8aW1hZ2UvcG5nfGgyZi9oMTgvOTg3MTA4MzM3MjU3NC5wbmd8YzNkNGE2YjVjZTJhZDczYzExZDU1MjJiNTQ2ZGJmZWM0ZTAzODRiNGZkNGVlOTZjMWZkOWY3ZDQ3Mjg3MjdkMg" />
+          <img src="https://http2.mlstatic.com/D_Q_NP_883789-MLA29271165898_012019-AE.webp" />
+          <h2>Samsung</h2>
+          <img src="" />
+          <img src="" />
+          <img src="" />
+        </div>
+      </div>
     </div>
   );
 }
