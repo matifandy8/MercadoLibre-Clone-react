0a2f2f574b02b7b22ab4fbb4926b48130ba38661
matifandy <matifandy@gmail.com>
2020-11-19 18:40:13 -0300

    brandstore

--- src/BrandStore.js
+++ src/BrandStore.js
@@ -40,19 +40,19 @@ function BrandStore() {
           id="4903850"
           title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
           price={199.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          image="https://http2.mlstatic.com/D_NQ_NP_653855-MLU42819496963_072020-W.webp"
         />
         <Product
           id="23445930"
           title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
           price={98.99}
-          image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          image="https://http2.mlstatic.com/D_NQ_NP_850084-MLU42183823481_062020-W.webp"
         />
         <Product
           id="3254354345"
           title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
           price={598.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          image="https://http2.mlstatic.com/D_NQ_NP_789366-MLU42138628966_062020-W.webp"
         />
       </div>
       <div className="brandStore__row"></div>
