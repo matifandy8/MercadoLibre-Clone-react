9c99af8dd4a6610e6d2925af638adec5cd6ccdbc
matifandy <matifandy@gmail.com>
2020-11-25 12:00:01 -0300

    style productDetails

--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./Product.css";
 import data from "./data";
+import { Link } from "react-router-dom";
 
 console.log(data);
 
 function Product(props) {
   const { product } = props;
   return (
-    <div className="products">
-      <div key={product._id} className="product">
-        <a href={`/product/${product._id}`}>
-          <img src={product.image} alt="" />
-        </a>
-        <div className="product__info">
-          <p className="product__price">
-            <small>$</small>
-            <strong>{product.price}</strong>
-          </p>
-          <p>{product.name}</p>
-          <button className="product__button">Comprar ahora</button>
+    <Link to={`/product/${product._id}`}>
+      <div className="products">
+        <div key={product._id} className="product">
+          <img src={product.image} />
+          <div className="product__info">
+            <p className="product__price">
+              <small>$</small>
+              <strong>{product.price}</strong>
+            </p>
+            <p>{product.name}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ProductDetails";
+import "./ProductDetails.css";
 import data from "./data";
 import Header from "./Header";
 
@@ -11,9 +11,23 @@ function ProductDetails(props) {
   return (
     <div className="productDetails">
       <Header />
-      <div>
+      <div className="productDetails__top">
+        <img
+          className="productDetails__img"
+          src="https://http2.mlstatic.com/storage/official-stores-images/lenovo/background201709180200.jpg"
+        />
+        <img
+          className="productDetails__brandimg"
+          src="https://http2.mlstatic.com/D_Q_NP_897210-MLA26014325691_092017-T.webp"
+        />
+      </div>
+      <div className="productDetails__info">
         <img src={product.image} />
-        <h1>{product.name}</h1>
+        <div className="productDetails__price">
+          <h1>{product.name}</h1>
+          <strong>${product.price}</strong>
+          <button>Comprar ahora</button>
+        </div>
       </div>
     </div>
   );
