b424583c0a927bd39a597450f57c561c35a5f9c1
matifandy <matifandy@gmail.com>
2020-11-29 19:39:07 -0300

    redux productdetails

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ProductDetails.css";
-import data from "./data";
 import Header from "./Header";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingBox from "./LoadingBox";
+import MessageBox from "./MessageBox";
+import { detailsProduct } from "./actions/productActions";
 
 function ProductDetails(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div>Product Not Found</div>;
-  }
+  const productDetails = useSelector((state) => state.productDetails);
+  const productId = props.match.params.id;
+  const dispatch = useDispatch();
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
-    <div className="productDetails">
-      <Header />
-      <div className="productDetails__top">
-        <img
-          className="productDetails__img"
-          src="https://http2.mlstatic.com/storage/official-stores-images/lenovo/background201709180200.jpg"
-        />
-        <img
-          className="productDetails__brandimg"
-          src="https://http2.mlstatic.com/D_Q_NP_897210-MLA26014325691_092017-T.webp"
-        />
-      </div>
-      <div className="productDetails__info">
-        <img src={product.image} />
-        <div className="productDetails__price">
-          <h1>{product.name}</h1>
-          <strong>${product.price}</strong>
-          <button className="productDetails__button">Comprar ahora</button>
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="productDetails">
+          <Header />
+          <div className="productDetails__top">
+            <img
+              className="productDetails__img"
+              src="https://http2.mlstatic.com/storage/official-stores-images/lenovo/background201709180200.jpg"
+            />
+            <img
+              className="productDetails__brandimg"
+              src="https://http2.mlstatic.com/D_Q_NP_897210-MLA26014325691_092017-T.webp"
+            />
+          </div>
+          <div className="productDetails__info">
+            <img src={product.image} />
+            <div className="productDetails__price">
+              <h1>{product.name}</h1>
+              <strong>${product.price}</strong>
+              <button className="productDetails__button">Comprar ahora</button>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,5 +1,8 @@
 import Axios from "axios";
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCESS,
@@ -16,5 +19,23 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_SUCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+  try {
+    const { data } = await Axios.get(
+      `http://localhost:1984/products/${productId}`
+    );
+    dispatch({ type: PRODUCT_DETAILS_SUCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
